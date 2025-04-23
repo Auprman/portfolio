480eb9af94ae5745cb739af3d0b3f76dde4fde3c
Felix Oppermann <felixoppermann@freenet.de>
2025-04-23 21:15:31 +0200

    added fotos and logos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AtfComponent } from "../sections/atf/atf.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AtfComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
